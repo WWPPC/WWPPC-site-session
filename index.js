83e444b8161a1744e657baef13054abd8e9c078d
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2024-06-29 19:39:50 -0400

    fix origin checks

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import createHost from 'cross-domain-storage/host';
 
 createHost([
     {
-        origin: 'http://wwppc.tech',
+        origin: 'https://wwppc.tech',
         allowedMethods: ['get', 'set', 'remove']
     },
     {
-        origin: 'http://math.wwppc.tech',
+        origin: 'https://math.wwppc.tech',
         allowedMethods: ['get', 'set', 'remove']
     }
 ]);
